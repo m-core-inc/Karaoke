fb80e320ec66e378ef2fb1123775ede6708ee8df
Bryan <Bwong727@gmail.com>
2017-02-24 20:45:43 -0500

    added new code to insert_song and insert_song_obj and other lines

--- app/controllers/mongo_controller.py
+++ app/controllers/mongo_controller.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient, errors, TEXT
+from app.models.song import Song
 
 
 class MongoController(MongoClient):
@@ -19,16 +20,39 @@ class MongoController(MongoClient):
         self.client = MongoClient(connectionString)[self.CONST_DB_NAME]
         self.songsCollections = self.client.songs
         self.songsCollections.create_index([(self.CONST_PROPERTY_TITLE, 1),
-                                           (self.CONST_PROPERTY_ARTIST, 1)],
-                                          default_language='english')
+                                           (self.CONST_PROPERTY_ARTIST, 1)], default_language='english')
+
+    def get_song(self, title, artist):
+
+        space = []
 
     def insert_song(self, title, artist, youtube):
 
-        self.songsCollections.insert_one({self.CONST_PROPERTY_TITLE: title,
-                                          self.CONST_PROPERTY_ARTIST: artist,
-                                          self.CONST_PROPERTY_YOUTUBE: youtube})
+        song = {self.CONST_PROPERTY_TITLE: title,
+                self.CONST_PROPERTY_ARTIST: artist,
+                self.CONST_PROPERTY_YOUTUBE: youtube}
+
+        return self.songsCollections.update({self.CONST_PROPERTY_TITLE: title, self.CONST_PROPERTY_ARTIST: artist},
+                                     song,
+                                     True)
+
+    def insert_song_obj(self, song):
+
+        title = song.get_title()
+        artist = song.get_artist()
+        youtube = song.get_link()
+
+        return self.insert_song(title, artist, youtube)
+
+    def remove_song(self, title, artist):
+
+        song = {}
 
 
 mongo = MongoController("localhost", "27017", "admin", "pa55word")
 
-mongo.insert_song("Capital Cites", "Safe and sound", "http://www.youtube.com/watch?v=47dtFZ8CFo8")
+mongo.insert_song("Safe and sound", "Capital Cites", "http://www.youtube.com/watch?v=47dtFZ8CFo8")
+
+song = Song("In The End", "Linkin Park", "www.youtube.com/watch?v=eVTXPUF4Oz4")
+
+mongo.insert_song_obj(song)
