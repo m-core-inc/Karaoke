e9d6016728f9cdce8c6746adb81023bcd28f0f67
Bryan <Bwong727@gmail.com>
2017-03-07 21:50:50 -0500

    added the get all songs function

--- app/controllers/mongo_controller.py
+++ app/controllers/mongo_controller.py
@@ -22,6 +22,10 @@ class MongoController(MongoClient):
         self.songsCollections.create_index([(self.CONST_PROPERTY_TITLE, 1),
                                            (self.CONST_PROPERTY_ARTIST, 1)], default_language='english')
 
+    def get_all_songs(self):
+
+        return self.songsCollections.find()[:50]
+
     def get_song(self, title, artist):
 
         return self.songsCollections.find_one({self.CONST_PROPERTY_TITLE: title, self.CONST_PROPERTY_ARTIST: artist})
@@ -45,17 +49,28 @@ class MongoController(MongoClient):
 
     def remove_song(self, title, artist):
 
-        return self.songsCollections.remove({self.CONST_PROPERTY_TITLE: title, self.CONST_PROPERTY_ARTIST: artist}, True)
+        return self.songsCollections.remove({self.CONST_PROPERTY_TITLE: title, self.CONST_PROPERTY_ARTIST: artist},
+                                            True)
 
+song = Song("Safe and Sound", "Capital Cites", "www.youtube.com/watch?v=47dtFZ8CFo8")
+song2 = Song("In The End", "Linkin Park", "www.youtube.com/watch?v=eVTXPUF4Oz4")
+song3 = Song("Beautiful Now", "Zedd", "www.youtube.com/watch?v=n1a7o44WxNo")
+song4 = Song("Clocks", "Cold Play", "www.youtube.com/watch?v=d020hcWA_Wg")
+song5 = Song("Best Day Of My Life", "American Authors", "www.youtube.com/watch?v=Y66j_BUCBMY")
 
 mongo = MongoController("localhost", "27017", "admin", "pa55word")
 
-mongo.insert_song("Safe and sound", "Capital Cites", "http://www.youtube.com/watch?v=47dtFZ8CFo8")
-
-song = Song("In The End", "Linkin Park", "www.youtube.com/watch?v=eVTXPUF4Oz4")
-
 mongo.insert_song_obj(song)
+mongo.insert_song_obj(song2)
+mongo.insert_song_obj(song3)
+mongo.insert_song_obj(song4)
+mongo.insert_song_obj(song5)
 
-print(mongo.get_song("Safe and sound", "Capital Cites"))
+# print(mongo.get_song("Safe and sound", "Capital Cites"))
 
-print(mongo.remove_song("In The End", "Linkin Park"))
+# print(mongo.remove_song("In The End", "Linkin Park"))
+
+stored = mongo.get_all_songs()
+
+for item in stored:
+    print(item)
